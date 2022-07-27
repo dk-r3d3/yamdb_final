4e4bfc8236347d7af954db873f6c9d0dbb4276d7
Дмитрий Коротеев <blessdevil@yandex.ru>
2022-07-27 20:21:04 +0700

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,7 +2,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from datetime import timedelta
-from pickle import FALSE
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
